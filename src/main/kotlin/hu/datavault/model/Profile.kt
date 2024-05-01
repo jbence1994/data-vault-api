a7e5fd812198af86421b6f1f23a727bcccc8ca45
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-05-01 15:45:18 +0200

    Transform Profile into a JPA entity.

--- src/main/kotlin/hu/datavault/model/Profile.kt
+++ src/main/kotlin/hu/datavault/model/Profile.kt
@@ -1,14 +1,32 @@
 package hu.datavault.model
 
+import hu.datavault.constant.TableSchemaConstants.TABLE_NAME_PROFILES
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = TABLE_NAME_PROFILES)
 data class Profile(
+
+    @Id
     var id: String = "",
+
     var firstName: String = "",
+
     var middleName: String? = null,
+
     var lastName: String = "",
+
     var gender: Int = 0,
-    var birthDate: String = "",
+
+    var birthDate: String? = null,
+
     var phone: String? = null,
+
     var email: String? = null,
+
     var photo: String? = null,
+
     var createdDateTime: String? = null,
 )
