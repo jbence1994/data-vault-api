b231a114223e49280b6c9bf5cf582354f03687d3
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2024-05-04 15:07:18 +0200

    Replace nullable fields (#19)

--- src/main/kotlin/hu/datavault/extension/ProfileInputExtensions.kt
+++ src/main/kotlin/hu/datavault/extension/ProfileInputExtensions.kt
@@ -1,22 +1,26 @@
 package hu.datavault.extension
 
+import hu.datavault.constant.MiscellaneousConstants.NOT_AVAILABLE
 import hu.datavault.model.Profile
 import hu.datavault.model.ProfileInput
-import java.time.LocalDateTime
-import java.util.UUID
 
 fun ProfileInput.toProfile(): Profile {
+    val middleName = if (this.middleName.isNullOrEmpty()) NOT_AVAILABLE else this.middleName
+    val nationality = if (this.nationality.isNullOrEmpty()) NOT_AVAILABLE else this.nationality
+    val birthDate = if (this.birthDate.isNullOrEmpty()) NOT_AVAILABLE else this.birthDate
+    val birthPlace = if (this.birthPlace.isNullOrEmpty()) NOT_AVAILABLE else this.birthPlace
+    val phone = if (this.phone.isNullOrEmpty()) NOT_AVAILABLE else this.phone
+    val email = if (this.email.isNullOrEmpty()) NOT_AVAILABLE else this.email
+
     return Profile(
-        id = UUID.randomUUID().toString(),
         firstName = this.firstName,
-        middleName = this.middleName,
+        middleName = middleName,
         lastName = this.lastName,
         gender = this.gender,
-        birthDate = this.birthDate,
-        birthPlace = this.birthPlace,
-        phone = this.phone,
-        email = this.email,
-        photo = this.photo,
-        createdDateTime = LocalDateTime.now().toString(),
+        nationality = nationality,
+        birthDate = birthDate,
+        birthPlace = birthPlace,
+        phone = phone,
+        email = email,
     )
 }
