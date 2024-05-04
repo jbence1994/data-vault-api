743f421744e4ef6b3f133b0c3132bc2e6d616398
jbence1994 <juhasz.bence.zsolt@gmail.com>
2024-05-04 15:03:17 +0200

    Add empty checks.

--- src/main/kotlin/hu/datavault/extension/ProfileInputExtensions.kt
+++ src/main/kotlin/hu/datavault/extension/ProfileInputExtensions.kt
@@ -5,15 +5,22 @@ import hu.datavault.model.Profile
 import hu.datavault.model.ProfileInput
 
 fun ProfileInput.toProfile(): Profile {
+    val middleName = if (this.middleName.isNullOrEmpty()) NOT_AVAILABLE else this.middleName
+    val nationality = if (this.nationality.isNullOrEmpty()) NOT_AVAILABLE else this.nationality
+    val birthDate = if (this.birthDate.isNullOrEmpty()) NOT_AVAILABLE else this.birthDate
+    val birthPlace = if (this.birthPlace.isNullOrEmpty()) NOT_AVAILABLE else this.birthPlace
+    val phone = if (this.phone.isNullOrEmpty()) NOT_AVAILABLE else this.phone
+    val email = if (this.email.isNullOrEmpty()) NOT_AVAILABLE else this.email
+
     return Profile(
         firstName = this.firstName,
-        middleName = this.middleName ?: NOT_AVAILABLE,
+        middleName = middleName,
         lastName = this.lastName,
         gender = this.gender,
-        nationality = this.nationality ?: NOT_AVAILABLE,
-        birthDate = this.birthDate ?: NOT_AVAILABLE,
-        birthPlace = this.birthPlace ?: NOT_AVAILABLE,
-        phone = this.phone ?: NOT_AVAILABLE,
-        email = this.email ?: NOT_AVAILABLE,
+        nationality = nationality,
+        birthDate = birthDate,
+        birthPlace = birthPlace,
+        phone = phone,
+        email = email,
     )
 }
